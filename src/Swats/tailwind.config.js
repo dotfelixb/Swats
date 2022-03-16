af0252decc5b9eecd7c5c12c813f285f60568bef
Kwaku.Boamah <kwaku.boamah@petraonline.com>
2022-03-16 13:27:19 +0000

    build tailwindcss offline

---
+++ src/Swats/tailwind.config.js
@@ -0,0 +1,26 @@
+﻿const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: [
+        "./Views/**/*.{html,cshtml}",
+        "./Shared/**/*.{html,cshtml}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    'Segoe UI',
+                    'Calibri',
+                    'Helvetica',
+                    ...defaultTheme.fontFamily.sans
+                ],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
+}
