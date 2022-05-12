c01dbccf6f5923a649a1b7fbdad41c6c3066b23e
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-05-12 12:17:40 +0000

    add email.list route

---
+++ src/Keis.Web/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keis.Data.Repository.EmailManager.Interfaces;
+using Keis.Model.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Keis.Web.Controllers
+{
+    public class EmailController : MethodController
+    {
+        private readonly IEmailRepository _emailRepository;
+
+        public EmailController(IEmailRepository emailRepository)
+        {
+            _emailRepository = emailRepository;
+        }
+        // GET: email.list
+        [AllowAnonymous]
+        [HttpGet("emails.list")]
+        public async Task<ActionResult<IEnumerable<EmailMessage>>> GetMails()
+        {
+            var emails = await _emailRepository.GetEmails();
+            return Ok(emails);
+        }
+
+    }
+}
+
