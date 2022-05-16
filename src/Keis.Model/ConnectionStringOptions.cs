aed218c4bdad1eed0031aacccee6276fb039f82c
dotfelix <dotfelixb@gmail.com>
2022-05-16 12:33:38 +0000

    Revert "Dev"

--- src/Keis.Model/ConnectionStringOptions.cs
+++ src/Keis.Model/ConnectionStringOptions.cs
@@ -9,14 +9,3 @@ public class SecurityKeyOptions
 {
     public string Bearer { get; set; }
 }
-
-public class EmailStringOptions
-{
-    public string ImapServer { get; set; }
-
-    public string ImapPort { get; set; }
-
-    public string ImapUsername { get; set; }
-
-    public string ImapPassword { get; set; }
-}
